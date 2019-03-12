e501c803de16457d3e4f3f466a0d37adf04d50c2
SenkiAlphonse <the.nagy.kriszta@gmail.com>
2019-03-12 11:51:37 +0100

    init

---
+++ src/main/java/com/hematac/fencingstats/FencingstatsApplication.java
@@ -0,0 +1,13 @@
+package com.hematac.fencingstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FencingstatsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FencingstatsApplication.class, args);
+  }
+
+}
