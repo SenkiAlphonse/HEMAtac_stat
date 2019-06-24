b02d85171a0773181cf56060a8df81f5d14356a3
SenkiAlphonse <Outrageous.13>
2019-06-24 15:29:01 +0200

    fencerDtoDisplay created. FencerRepositoryMethods added

---
+++ src/main/java/com/hematac/fencingstats/dto/FencerDtoDisplay.java
@@ -0,0 +1,12 @@
+package com.hematac.fencingstats.dto;
+
+public class FencerDtoDisplay {
+    public long id;
+    public String name;
+    public String lastCoach;
+    public String nationality;
+    public String handedness;
+    private String lastClub;
+    private String lastTeam;
+
+}
--- src/main/java/com/hematac/fencingstats/repository/sportentityrepository/FencerRepository.java
+++ src/main/java/com/hematac/fencingstats/repository/sportentityrepository/FencerRepository.java
@@ -1,9 +1,19 @@
 package com.hematac.fencingstats.repository.sportentityrepository;
 
 import com.hematac.fencingstats.models.sportentities.Fencer;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface FencerRepository extends CrudRepository<Fencer, Long> {
+public interface FencerRepository extends PagingAndSortingRepository<Fencer, Long> {
+
+    List<Fencer> findAll();
+    Fencer findById(long id);
+
+    List<Fencer> findAllByOrderByNameAsc(Pageable pageable);
+    List<Fencer> findAllByNameContainingOrderByNationalityAsc(Pageable pageable);
 }
